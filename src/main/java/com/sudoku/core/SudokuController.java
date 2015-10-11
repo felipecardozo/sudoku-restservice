0955c1bedc76f49fd4ae95c230e2e08ca5f4a10c
Felipe Cardozo <pypelyncar@hotmail.com>
2015-10-10 23:50:23 -0500

    adds servlet configuration, deletes some files not anymore needed and implements the rest controller to handle the requests

--- src/main/java/com/sudoku/core/SudokuController.java
+++ src/main/java/com/sudoku/core/SudokuController.java
@@ -1,8 +1,39 @@
 package com.sudoku.core;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.sudoku.core.model.MovementApproved;
+import com.sudoku.core.model.Sudoku;
+import com.sudoku.core.service.SudokuService;
+
+@RestController
+@RequestMapping("/sudoku")
 public class SudokuController {
+	
+	@Autowired
+	private SudokuService sudokuService;
+	
+	@RequestMapping("/create")
+	public Sudoku createSudoku(){
+		return sudokuService.createSudoku();
+	}
+	
+	@RequestMapping("/put-number/row/{row}/col/{col}/number/{number}")
+	public MovementApproved putNumber( @PathVariable("row") int row, @PathVariable("col") int col, @PathVariable("number") int number ){
+		return sudokuService.setMovement(col, row, number);
+	}
+	
+	@RequestMapping("/get-board")
+	public Sudoku getBoard( ){
+		return sudokuService.getBoard();
+	}
+	
+	@RequestMapping("/solve")
+	public Sudoku solve(){
+		return sudokuService.solve();
+	}
 
 }
