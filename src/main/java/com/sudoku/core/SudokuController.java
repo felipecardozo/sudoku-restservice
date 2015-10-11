a817f2e87a4f6eb35e93c0fd7565a78fcb5c17c4
Felipe Cardozo <pypelyncar@hotmail.com>
2015-10-11 00:02:08 -0500

    adds some examples to test the rest services

--- src/main/java/com/sudoku/core/SudokuController.java
+++ src/main/java/com/sudoku/core/SudokuController.java
@@ -16,21 +16,25 @@ public class SudokuController {
 	@Autowired
 	private SudokuService sudokuService;
 	
+	//http://localhost:8080/core/sudoku/create
 	@RequestMapping("/create")
 	public Sudoku createSudoku(){
 		return sudokuService.createSudoku();
 	}
 	
+	//http://localhost:8080/core/sudoku/put-number/row/1/col/1/number/3
 	@RequestMapping("/put-number/row/{row}/col/{col}/number/{number}")
 	public MovementApproved putNumber( @PathVariable("row") int row, @PathVariable("col") int col, @PathVariable("number") int number ){
 		return sudokuService.setMovement(col, row, number);
 	}
 	
+	//http://localhost:8080/core/sudoku/get-board
 	@RequestMapping("/get-board")
 	public Sudoku getBoard( ){
 		return sudokuService.getBoard();
 	}
 	
+	//http://localhost:8080/core/sudoku/solve
 	@RequestMapping("/solve")
 	public Sudoku solve(){
 		return sudokuService.solve();
